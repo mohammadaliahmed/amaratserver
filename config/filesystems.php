126ac66113a99f07636fb4a113aa2e8aba8317f1
ali <m.aliahmed0@gmail.com>
2022-12-17 13:11:32 +0500

    a

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,17 +32,31 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('/'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
+            'root' => storage_path('app'),
+            'url' => env('APP_URL') . '/storage',
+            'throw' => false,
         ],
-
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
+            'throw' => false,
         ],
+
+//        'local' => [
+//            'driver' => 'local',
+//            'root' => storage_path('/'),
+//            'url' => env('APP_URL').'/storage',
+//            'visibility' => 'public',
+//        ],
+//
+//        'public' => [
+//            'driver' => 'local',
+//            'root' => storage_path('app/public'),
+//            'url' => env('APP_URL').'/storage',
+//            'visibility' => 'public',
+//        ],
 
         's3' => [
             'driver' => 's3',
