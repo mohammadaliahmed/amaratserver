0e3e2618f1bb3bf0f3266519fac0fdbd8940bdf0
ali <m.aliahmed0@gmail.com>
2023-01-18 13:17:34 +0500

    a

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -21,7 +21,7 @@ class AppController extends Controller
             $customer_has_mail = Customer::Where('email', $request->email)->count();
             if ($customer_has_mail != 0) {
                 return response()->json([
-                    'code' => Response::HTTP_FORBIDDEN, 'message' =>"Email already exists"
+                    'code' => Response::HTTP_FORBIDDEN, 'message' => "Email already exists"
                 ], Response::HTTP_FORBIDDEN);
             }
         }
@@ -54,11 +54,11 @@ class AppController extends Controller
                     ->where('password', md5($request->password))->first();
                 if (!isset($customer)) {
                     return response()->json([
-                        'code' => Response::HTTP_FORBIDDEN,  'message' => "Wrong password"
+                        'code' => Response::HTTP_FORBIDDEN, 'message' => "Wrong password"
                     ], Response::HTTP_FORBIDDEN);
                 } else {
                     return response()->json([
-                        'code' => Response::HTTP_OK,  'message' => "",'customer'=>$customer
+                        'code' => Response::HTTP_OK, 'message' => "", 'customer' => $customer
                     ], Response::HTTP_OK);
                 }
 
@@ -67,50 +67,53 @@ class AppController extends Controller
 
     }
 
-    public function ListProducts(){
-        $products=Product::all();
-        foreach ($products as $product){
-            $unit=Unit::where('id',$product->unit_id)->pluck('shortname');
-            if(sizeof($unit)>0){
-                $product->unit=$unit[0];
-            }else{
-                $product->unit="";
+    public function ListProducts()
+    {
+        $products = Product::all();
+        foreach ($products as $product) {
+            $unit = Unit::where('id', $product->unit_id)->pluck('shortname');
+            if (sizeof($unit) > 0) {
+                $product->unit = $unit[0];
+            } else {
+                $product->unit = "";
             }
 
         }
         return response()->json([
-            'code' => Response::HTTP_OK,  'message' => "",'products'=>$products
+            'code' => Response::HTTP_OK, 'message' => "", 'products' => $products
         ], Response::HTTP_OK);
     }
 
-    public function PlaceOrder(Request $request){
+    public function PlaceOrder(Request $request)
+    {
 
-        $latest= Sale::orderBy('created_at', 'desc')->first();
-        $invoice_id=$latest ? $latest->invoice_id + 1 : 1;
+        $latest = Sale::orderBy('created_at', 'desc')->first();
+        $invoice_id = $latest ? $latest->invoice_id + 1 : 1;
 
-        $sale=new Sale();
-        $sale->customer_id=$request->user['id'];
-        $sale->invoice_id=$invoice_id;
-        $sale->branch_id=0;
-        $sale->cash_register_id=0;
-        $sale->status=0;
-        $sale->created_by=1;
+        $sale = new Sale();
+        $sale->customer_id = $request->userId;
+        $sale->invoice_id = $invoice_id;
+        $sale->branch_id = 0;
+        $sale->cash_register_id = 0;
+        $sale->status = 0;
+        $sale->created_by = 1;
         $sale->save();
-
+//
         foreach ($request->items as $key => $value) {
-            $product_id = $value['id'];
+            $product = Product::find($key);
+            $product_id = $key;
             $selleditems = new SelledItems();
             $selleditems->sell_id = $sale->id;
-            $selleditems->product_id = $product_id;
-            $selleditems->price = $value['sale_price'];
-            $selleditems->quantity = $value['quantity'];
+            $selleditems->product_id = $key;
+            $selleditems->price = $product->sale_price;
+            $selleditems->quantity = $value;
             $selleditems->tax_id = 0;
             $selleditems->tax = 0;
 
             $selleditems->save();
         }
         return response()->json([
-            'code' => Response::HTTP_OK,  'message' => "success"
+            'code' => Response::HTTP_OK, 'message' => "success"
         ], Response::HTTP_OK);
 
     }
