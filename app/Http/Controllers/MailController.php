bd9839817ea9528dc46abd8fa65e6f5f507d2d66
ali <m.aliahmed0@gmail.com>
2023-01-26 12:07:21 +0500

    a

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -13,7 +13,7 @@ class MailController extends Controller
         $subject = "Invited to join a group";
 
         Mail::send('emails.testmail', ['groupcode' => 'sdfsdfs'], function ($message) use ($request, $subject) {
-            $message->from('noreply@yoolah.com', 'Yoolah');
+            $message->from('info@amaratmaterials.com', 'Amarat Materials');
             $message->subject($subject);
             $message->to("m.aliahmed0@gmail.com");
         });
--- config/mail.php
+++ config/mail.php
@@ -38,7 +38,7 @@ return [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'mailtrap.io'),
             'port' => env('MAIL_PORT', 2525),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'encryption' => env('MAIL_ENCRYPTION', ''),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
