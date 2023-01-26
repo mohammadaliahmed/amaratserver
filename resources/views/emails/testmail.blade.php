eaa4fb37efa8a759febcc22e7e6c19e73aa4270f
ali <m.aliahmed0@gmail.com>
2023-01-26 15:35:52 +0500

    a

--- resources/views/emails/testmail.blade.php
+++ resources/views/emails/testmail.blade.php
@@ -1,48 +1,49 @@
 <html>
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
 </head>
 <body>
 
-<div class="border m-3 p-3 w-50" style="background-color: #ececec">
+<div  style="background-color: #ececec;width: 400px;padding: 10px;margin: 20px;border-radius: 20px">
 
     <center><h3>Order Confirmation</h3>
 
 
-        Dear <strong>{{$sale->customer->name}}</strong>, thank you for your order!<br>
+        Dear <strong>{{$sale->customer->name}}</strong>,
+
+        Thank you for your order!<br><br>
+
         We have received your order and we will contact you as soon.<br>
         You can find your purchase information below
+        <hr>
 
-        <br>
         <h3>Order Summary</h3>
     </center>
-    <div class="row">
-        @foreach($sale->items as $item)
-            <div class="d-flex justify-content-center">
 
-                <div class="col-12" style="width: 500px">
-                    <div class="p-1 m-3" style="background-color: #ffffff">
-                        <div class="d-flex">
-                            <img src="{{url('/')}}/storage/{{$item->product->image}}" width="100" height="100"/>
-                            <div class="m-2 align-left">
-                                <strong>{{$item->product->name}}</strong>
-                                <br>
-                                <strong> Rs {{$item->product->sale_price}}</strong>
-                                <br>
-                                <strong>Qty: {{$item->quantity}}</strong>
-                            </div>
-                        </div>
+    @foreach($sale->items as $item)
+
+            <div  style="background-color: #ffffff;width: 350px;margin: 20px">
+                <div style=" display: flex;">
+                    <img style="margin: 5px" src="{{url('/')}}/storage/{{$item->product->image}}" width="100" height="100"/>
+                    <div style="margin: 10px">
+                        <strong>{{$item->product->name}}</strong>
+                        <br>
+                        <strong> Rs {{$item->product->sale_price}}</strong>
+                        <br>
+                        <strong>Qty: {{$item->quantity}}</strong>
                     </div>
                 </div>
-
             </div>
-        @endforeach
-    </div>
+
+
+    @endforeach
+
     <center>
+        <hr>
+
         <h3>Order Total: Rs {{$sale->getTotal()}}</h3>
 
+        <hr>
 
         <h3>Shipping address</h3>
         {{$sale->customer->name}}<br>
