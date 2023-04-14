6706afcb44978373bd09f1eb2acfba6f8fb925e3
arslan ahmed <arslanahmed430@gmail.com>
2023-04-14 21:36:23 +0500

    a

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -519,7 +519,7 @@ class SaleController extends Controller
         //Set your logo
         $logo = \App\Models\Utility::get_file('uploads/logo/');
         $company_logo = \App\Models\Utility::get_superadmin_logo();
-        $img = asset($logo . 'logo-black.png'));
+        $img = asset($logo . 'logo-black.png');
 
         return view('sales.templates.' . $template, compact('sale', 'preview', 'color', 'font_color', 'settings', 'user', 'customerdetails', 'img'));
     }
